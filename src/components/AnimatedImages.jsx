b14f3aa497d81c0cd7f2536cf6995b86668dcc0b
YasanthFlyxto <143312439+YasanthFlyxto@users.noreply.github.com>
2024-10-14 19:32:28 +0530

    update latest

--- src/components/AnimatedImages.jsx
+++ src/components/AnimatedImages.jsx
@@ -3,27 +3,27 @@ import { motion } from 'framer-motion';
 
 const AnimatedImages = () => {
   const images = [
-    { src: "/header/trim.png", className: "bottom-3 rotate-12" },
-    { src: "/header/camera.png", className: "left-72 -rotate-12 bottom-40" },
-    { src: "/header/video-camera.png", className: "right-72 rotate-12 bottom-40" },
-    { src: "/header/writing.png", className: "bottom-3 right-0 -rotate-12" }
+    { src: "/header/trim.png", class: "lg:bottom-3 left-12 lg:left-0 lg:rotate-12 sm:hidden lg:block" },
+    { src: "/header/camera.png", class: "lg:left-72 left-80 -rotate-12 lg:bottom-40 bottom-24 sm:hidden lg:block" },
+    { src: "/header/video-camera.png", class: "lg:right-72 rotate-12 lg:bottom-40 bottom-24 right-80 sm:hidden lg:block" },
+    { src: "/header/writing.png", class: "lg:bottom-3 lg:right-0 right-12 lg:-rotate-12 sm:hidden lg:block" }
   ];
 
   return (
-    <div className="relative">
+    <div class="relative">
       {images.map((image, index) => (
         <motion.img
           key={index}
-          className={`absolute w-20 ${image.className}`}
+          class={`absolute w-14 sm:w-20 ${image.class}`}
           src={image.src}
           alt=""
           initial={{ y: '100%', opacity: 0, rotate: 0 }}
-          animate={{ y: 0, opacity: 1, rotate: image.className.includes('-rotate') ? -12 : 12 }}
-          transition={{ 
-            duration: 0.8, 
-            delay: index * 0.2, 
-            type: 'spring', 
-            stiffness: 120 
+          animate={{ y: 0, opacity: 1, rotate: image.class.includes('-rotate') ? -12 : 12 }}
+          transition={{
+            duration: 0.8,
+            delay: index * 0.2,
+            type: 'spring',
+            stiffness: 120
           }}
         />
       ))}
